b590cfda45288e50e0225e22aea3b7d00b5a4217
JChantrel <joerichantrel@gmail.com>
2020-12-09 16:54:44 +0100

    Create cmake_clean.cmake

---
+++ Software/thinger_client/install/raspberry/build/CMakeFiles/thinger.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/thinger.dir/src/main.cpp.o"
+  "CMakeFiles/thinger.dir/src/mmapGpio.cpp.o"
+  "thinger.pdb"
+  "thinger"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/thinger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
