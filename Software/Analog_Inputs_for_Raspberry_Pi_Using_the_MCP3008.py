d9691c737cb6a11b38e250fdae3be4760f0df23a
JChantrel <joerichantrel@gmail.com>
2021-01-06 10:20:35 +0100

    calibration

--- Software/Analog_Inputs_for_Raspberry_Pi_Using_the_MCP3008.py
+++ Software/Analog_Inputs_for_Raspberry_Pi_Using_the_MCP3008.py
@@ -22,47 +22,39 @@ print('Raw ADC Value: ', chan0.value)
 print('ADC Voltage: ' + str(chan0.voltage) + 'V')
 
 last_read = 0       # this keeps track of the last potentiometer value
-tolerance = 250     # to keep from being jittery we'll only change
-                    # volume when the pot has moved a significant amount
-                    # on a 16-bit ADC
-
-def remap_range(value, left_min, left_max, right_min, right_max):
-    # this remaps a value from original (left) range to new (right) range
-    # Figure out how 'wide' each range is
-    left_span = left_max - left_min
-    right_span = right_max - right_min
-
-    # Convert the left range into a 0-1 range (int)
-    valueScaled = int(value - left_min) / int(left_span)
-
-    # Convert the 0-1 range into a value in the right range.
-    return int(right_min + (valueScaled * right_span))
+tolerance = 250     # to keep from being jittery we'll only change volume when the pot has moved a significant amount on a 16-bit ADC
+                    
+slope = 2.48
+intercept = 0
 
 while True:
     # we'll assume that the pot didn't move
-    trim_pot_changed = False
+    value_changed = False
 
     # read the analog pin
-    trim_pot = chan0.value
+    value = chan0.value
 
     # how much has it changed since the last read?
-    pot_adjust = abs(trim_pot - last_read)
+    value_adjust = abs(value - last_read)
 
-    if pot_adjust > tolerance:
-        trim_pot_changed = True
+    if value_adjust > tolerance:
+        value_changed = True
 
-    if trim_pot_changed:
+    if value_changed:
         # convert 16bit adc0 (0-65535) trim pot read into 0-100 volume level
-        set_volume = remap_range(trim_pot, 0, 65535, 0, 100)
+        voltage = chan0.voltage
+        
+        water_cont = ((1.0/voltage)*slope)+intercept
 
         # set OS volume playback volume
-        print('Volume = {volume}%' .format(volume = set_volume))
-        set_vol_cmd = 'sudo amixer cset numid=1 -- {volume}% > /dev/null' \
-        .format(volume = set_volume)
-        os.system(set_vol_cmd)
+        print('ADC = %i' % value)
+        print('Voltage = %.2f V' % voltage)
+        print('Water = %.2f cm^3/cm^3' % water_cont)
+        #set_vol_cmd = 'sudo amixer cset numid=1 -- {volume}% > /dev/null'.format(volume = voltage)
+        #os.system(set_vol_cmd)
 
         # save the potentiometer reading for the next loop
-        last_read = trim_pot
+        last_read = value
 
     # hang out and do nothing for a half second
     time.sleep(0.5)
