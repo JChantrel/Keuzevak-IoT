073835db834c9c4d23cf88ac2b26989502435dc8
Joeri Chantrel <74982195+JChantrel@users.noreply.github.com>
2021-01-07 12:23:45 +0100

    Update Button.py

--- Software/Hello-World/Button.py
+++ Software/Hello-World/Button.py
@@ -4,7 +4,7 @@ from time import sleep
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8,GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(10,GPIO.in, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(10,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
   button_state = GPIO.input(10)
