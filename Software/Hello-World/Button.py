4d186153aac93a347a3297636423f9d5c26dc85c
Joeri Chantrel <74982195+JChantrel@users.noreply.github.com>
2021-01-07 12:23:11 +0100

    Create Button.py

---
+++ Software/Hello-World/Button.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8,GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(10,GPIO.in, pull_up_down=GPIO.PUD_UP)
+
+while True:
+  button_state = GPIO.input(10)
+  if button_state == True:
+    GPIO.output(8,False)
+    sleep(0.2)
+   else:
+    GPIO.output(8,True)
